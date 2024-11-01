f7a377005a32e74ed968c6bce155d721f2af9747
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-11-01 16:39:05 -0300

    Add login (não implementado)

---
+++ WebHamburgueria/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHamburgueria.ViewModel
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
