dab811ba3583472d5eea5448c6c674f4262738f4
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-11-01 16:46:07 -0300

    Implementação incompleta de login

--- WebHamburgueria/ViewModel/LoginViewModel.cs
+++ WebHamburgueria/ViewModel/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace WebHamburgueria.ViewModel
     {
         public int Id { get; set; }
         public string Login { get; set; }
-        public string Senha { get; set; }
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Informe a senha.", AllowEmptyStrings = false)]
+        public string Password { get; set; }
     }
 }
