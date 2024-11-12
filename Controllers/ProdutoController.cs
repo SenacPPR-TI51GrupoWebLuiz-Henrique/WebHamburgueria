bc1d20491d83ef83b34a9786cef4fbfa4c51ee61
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-11-12 17:24:49 -0300

    Imagem produto ainda não implementado

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebHamburgueria.Models;
+
+namespace WebHamburgueria.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private dbhamburgueriaEntities db = new dbhamburgueriaEntities();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View(db.Produto.ToList());
+        }
+
+        // GET: Produto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produto/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Preco,Descricao,Ingredientes,Foto,Tipo")] Produto produto, Upload upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //*Salvar o produto na pasta
+                foreach (var file in upload.Files)
+                {
+                    if (file.ContentLength > 0)
+                    {
+                        var filename = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/img"), filename);
+                        file.SaveAs(path);
+                        produto.Foto
+                    }
+
+                    db.Produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(produto);
+        }
+
+        // GET: Produto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produto/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Preco,Descricao,Ingredientes,Foto,Tipo")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+        // GET: Produto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.Produto.Find(id);
+            db.Produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
