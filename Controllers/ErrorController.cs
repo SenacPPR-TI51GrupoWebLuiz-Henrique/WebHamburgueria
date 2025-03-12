bda41bdc4276d20b5800ff1fea3a5ab0296d20ce
Luiz H D Alecrim <163210265+Lu1zH3nr1qu3DA@users.noreply.github.com>
2025-03-12 16:53:09 -0300

    Adicionado: páginas de erro personalizadas.
    Co-Authored-By: HenriqueSF1 <180222228+HenriqueSF1@users.noreply.github.com>

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHamburgueria.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        // Página de erro padrão
+        public ActionResult Index()
+        {
+            ViewBag.ErrorCode = Response.StatusCode;
+            return View();
+        }
+
+        // Página para erros 404 - Não Encontrado
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            ViewBag.ErrorCode = Response.StatusCode;
+            return View();
+        }
+
+        // Página para erros 500 - Erro Interno do Servidor
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            ViewBag.ErrorCode = Response.StatusCode;
+            return View();
+        }
+    }
+}
